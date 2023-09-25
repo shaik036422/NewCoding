2c03dd57a69dcb50c9a5c27fd248c1976ff44d2c
shaiksha <shaiksha3622@gmail.com>
2023-09-25 12:59:00 +0530

    commit

---
+++ force-app/main/default/lwc/dataTable/dataTable.js
@@ -0,0 +1,15 @@
+import { LightningElement } from 'lwc';
+
+export default class DataTable extends LightningElement {
+    months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    dates = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30', '31'];
+    
+    /*salesData = {
+        'January': {
+            '01': 100, '02': 150, '03': 200 // Replace with your sales data for January
+        },
+        'February': {
+            '01': 120, '02': 180, '03': 220 // Replace with your sales data for February
+        },
+    };*/
+}
